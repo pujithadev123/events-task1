7ec0eacaf295ffef3f17fab2bfc5ebff68b35971
G.V.S.Pujitha <pujithagvs@gmail.com>
2024-11-08 19:27:31 +0530

    event first task was added

---
+++ 3images.js
@@ -0,0 +1,20 @@
+
+let j=document.getElementById("text")
+function c(){
+    j.textContent="I will definetly get the job"
+    j.style.backgroundColor="red"
+    j.style.fontSize="60px"
+    j.style.borderRadius="50%"
+    j.style.width="30vw"
+    j.style.height="50vh"
+    j.style.padding="100px"    
+}
+
+let k=document.getElementById("pics")
+function d(){
+    
+         k.innerHTML=`<img src="https://i0.wp.com/picjumbo.com/wp-content/uploads/amazing-stone-path-in-forest-free-image.jpg?w=600&quality=80" width="300" style="border-radius:30px;margin-top:40px;margin-left:30px" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2dVbLMzlaeJnL5C6RpZ8HLRECJhH6ILEGKg&s" width="300"  style="border-radius:100px;margin-top:40px;margin-left:30px"/>
+        <img src="https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&w=600" width="300"  style="border-radius:50%;margin-top:40px;margin-left:30px"/>  `
+        
+}
